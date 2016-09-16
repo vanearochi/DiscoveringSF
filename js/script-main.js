2852c0bf0da0fc9104148af974935534510994e7
Vanessa Arochi <vanearochi@gmail.com>
2016-09-15 19:18:50 -0700

    Added google directions, and functions for the correct display of it

--- js/script-main.js
+++ js/script-main.js
@@ -2,32 +2,36 @@
 var map;
 var placesList = [ 'Golden Gate Bridge', 'Angel Island', 'Golden Gate Park', 'Fort Point', 'Mount Davidson', 'Ferry Building', 'Coit Tower', 'Mission District', 'Castro Theatre', 'Alcatraz'];
 var placesContainer= ko.observable([
-					{label: "A", name:'Golden Gate Bridge', location:{lat: 37.8199286, lng: -122.47825510000001}, titleVisible:ko.observable(true)},
-					{label:"B", name:'Angel Island', location: {lat: 37.860909, lng: -122.43256819999999}, titleVisible:ko.observable(true)},
-					{label:"C", name:"Golden Gate Park", location: {lat: 37.7694208, lng: -122.48621379999997}, titleVisible:ko.observable(true)},
-					{label:"D", name:"Fort Point", location: {lat: 37.8105931, lng: -122.4771093}, titleVisible:ko.observable(true)},
-					{label:"E", name:"Mount Davidson", location: {lat: 37.73833330000001, lng: -122.4533333}, titleVisible:ko.observable(true)},
-					{label:"F",name:"San Francisco Ferry Building", location: {lat: 37.7955469, lng: -122.39341769999999}, titleVisible:ko.observable(true)},
-					{label:"G",name: "Coit Tower", location:{lat: 37.8023949, lng: -122.40582219999999}, titleVisible:ko.observable(true)},
-					{label:"H",name: "Mission District", location:{lat: 37.7598648, lng: -122.41479770000001}, titleVisible:ko.observable(true)},
-					{label:"I",name:"The Castro Theatre", location:{lat: 37.761992, lng: -122.43473590000002}, titleVisible:ko.observable(true)},
-					{label:"J",name:"Alcatraz Island", location:{lat: 37.8269775, lng: -122.4229555}, titleVisible:ko.observable(true)}
+					{ name:'Gol{den Gate Bridge', location:{lat: 37.8199286, lng: -122.47825510000001}, titleVisible:ko.observable(true)},
+					{ name:'Angel Island', location: {lat: 37.860909, lng: -122.43256819999999}, titleVisible:ko.observable(true)},
+					{ name:"Golden Gate Park", location: {lat: 37.7694208, lng: -122.48621379999997}, titleVisible:ko.observable(true)},
+					{ name:"Fort Point", location: {lat: 37.8105931, lng: -122.4771093}, titleVisible:ko.observable(true)},
+					{ name:"Mount Davidson", location: {lat: 37.73833330000001, lng: -122.4533333}, titleVisible:ko.observable(true)},
+					{name:"San Francisco Ferry Building", location: {lat: 37.7955469, lng: -122.39341769999999}, titleVisible:ko.observable(true)},
+					{name: "Coit Tower", location:{lat: 37.8023949, lng: -122.40582219999999}, titleVisible:ko.observable(true)},
+					{name: "Mission District", location:{lat: 37.7598648, lng: -122.41479770000001}, titleVisible:ko.observable(true)},
+					{name:"The Castro Theatre", location:{lat: 37.761992, lng: -122.43473590000002}, titleVisible:ko.observable(true)},
+					{name:"Alcatraz Island", location:{lat: 37.8269775, lng: -122.4229555}, titleVisible:ko.observable(true)}
 				]);
 
 
 
-var markerSelected=ko.observable("hola");
+var markerSelected=ko.observable("");
+var actualMarker;
 //suscribe it and as soon as the value change it calls the hide titles, hide markers from pass info function
 var labelContainer = ko.observable(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"])
 
 var passTheInfo;
 var koAddress = ko.observable("");
-
+//var label;
 
 //add a branch to make do the vegan app and search api and location in places
 
 
 function initMap(){
+
+	var directionsDisplay;
+
 
 	//Creating map
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -43,16 +47,19 @@ function initMap(){
 
 		var name = placesContainer()[i].name;
 		var position = placesContainer()[i].location;
-		createMarker(placesContainer()[i])
+		var counter = i + 1;
+		createMarker(placesContainer()[i], counter)
+
 	}
 
+	//var label = 1;
+
+	function createMarker(place, counter){
 
 
-	function createMarker(place){
-
-
-			var label = place.label;
+			var labelStr = i.toString()
 			var title = place.name;
+
 
 			// var latLng = data[0].geometry.location.toJSON();
 			// var placeName = data[0].name;
@@ -75,7 +82,7 @@ function initMap(){
 	    		title: title,
 	    		id: i,
 	    		label: {
-	    			text: label,
+	    			text: labelStr,
 	    			fontSize: "18px"
 	    		},
 	    		visible: true,
@@ -89,6 +96,7 @@ function initMap(){
 			infoWindow =  new google.maps.InfoWindow();
 	  		google.maps.event.addListener(marker, 'click', passTheInfo)
 	  		place.marker = marker;
+
 	  		//place.pLabel = label;
 	  		//console.log(place)
 
@@ -123,14 +131,14 @@ function initMap(){
 
 	askInfoToPlacesLibrary = function(marker){
 
-		console.log("success click from MV to maps")
+		//console.log("success click from MV to maps")
 		var name = marker.title
 		var placeServices = new google.maps.places.PlacesService(map);
 		placeServices.textSearch({query: name}, placeInformation)
 		marker.setAnimation(google.maps.Animation.BOUNCE);
 		function placeInformation(data, status){
-			console.log(data)
-			console.log()
+			//console.log(data)
+			//console.log()
 			var domElementForAdress = document.getElementById(name)
 
 			if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -138,8 +146,8 @@ function initMap(){
 				var placeInfo = data[0]
 				var address = placeInfo.formatted_address;
 				//console.log(domElementForAdress)
-				//console.log(address)
-
+				console.log(address)
+				koAddress("")
 				koAddress(address)
 
 
@@ -188,16 +196,8 @@ function initMap(){
 
 	askWikiInfo = function(address, marker){
 
-		wikiPromise = giveWikiInfo(marker.title)
-	    wikiPromise.then(function(data){
-	    		var wikiPlaceInfo = data.query.search[0].snippet
-	    		//console.log(marker.title)
-	    		openInfoWindow(marker, address, wikiPlaceInfo)
 
 
-
-
-	    })
 	    	//console.log(data)
 
 	}
@@ -206,17 +206,120 @@ function initMap(){
 		markerSelected(this);
 		//console.log(markerSelected())
 		hideMarkers(this)
-		//console.log(this)
 
-		//console.log("click sucess")
-		//console.log(this.title)
-		//var name = this.title
-		//askInfoToPlacesLibrary(this)
-		//console.log(markerSelected)
+		askInfoToPlacesLibrary(this)
+
+
 
 
 
 	}
+
+	showDirections = function(geoposition){
+
+			console.log("sucess show dir callGoogleDirections")
+			map.setZoom(12)
+
+			var travelMode;
+			var latitude= geoposition.coords.latitude;
+			var longitud = geoposition.coords.longitude;
+			//console.log(marker)
+			var userLocation = {lat: latitude, lng: longitud}
+
+
+			//hidePlaces()
+
+
+	            var userMarker = new google.maps.Marker({
+        			map: map,
+        			position: userLocation,
+        			icon: {
+      				path: "M22-48h-44v43h16l6 5 6-5h16z",
+      				scale: 0.8,
+  					fillColor: '#1569C7',
+  					fillOpacity: 0.6,
+      				strokeColor: 'black'
+    				}
+  				});
+
+	            google.maps.event.addListener(userMarker, 'click', function(){
+	            	infoWindow.setContent("Your location")
+
+  					infoWindow.open(map, this)
+  				})
+
+
+  				//showRoute(userLocation)
+
+
+
+
+        	// function(e){
+
+        	// 	window.alert("Sorry we were unable to find your location");
+
+        	// 	//console.log(e)
+        	// });
+
+
+    		//console.log(currentLocation)
+    		console.log("sucess show route")
+    		var directionsService = new google.maps.DirectionsService();
+    		directionsDisplay = new google.maps.DirectionsRenderer();
+    		directionsDisplay.setMap(map);
+			directionsDisplay.setPanel(document.getElementById('steps'))
+
+				travelMode = document.getElementById('travelMode').value
+				console.log(markerSelected().position)
+				//console.log(selectedLocation.address())
+
+  				var request = {
+			    	origin: userLocation,
+			     	destination: markerSelected().position,
+
+			    	travelMode: google.maps.TravelMode[travelMode]
+		 		};
+
+		    directionsService.route(request, function(response, status) {
+		    	console.log(response)
+
+		    	if (status == google.maps.DirectionsStatus.OK) {
+		    		userMarker.setVisible(false)
+		    		markerSelected().setVisible(false)
+		      		directionsDisplay.setDirections(response);
+		      		//console.log("si")
+		      		var basePath = response.routes[0].legs[0];
+
+		      		var userLocationAdress = basePath.start_address;
+		      		var finalDestinaton = markerSelected.title;
+		      		var distance = basePath.distance.text;
+		      		var duration = basePath.duration.text
+
+
+
+
+
+		    	}
+		    	else if(status == google.maps.DirectionsStatus.ZERO_RESULTS){
+		    		alert("No route could be found between the origin and destination. Please try another travel mode")
+		    	}
+		    	else{
+		    		alert("Sorry there was an error:" + status)
+		    		console.log(status)
+
+		    	}
+	  		});
+	  		//timeToGetThere(userPosition)
+	  		var w = document.getElementById('travelMode').value;
+	  		console.log(w)
+
+
+
+    }
+
+
+
+
 
 	hideMarkers = function(marker){
 
@@ -242,6 +345,11 @@ function initMap(){
 
 		map.setZoom(12)
 		map.setCenter({ lat: 37.82, lng: -122.431297})
+		console.log(directionsDisplay)
+		if(directionsDisplay != undefined){
+			directionsDisplay.setMap(null)
+			directionsDisplay.setPanel(null)
+		}
 		for (var i = 0; i < placesContainer().length; i++) {
 			var place = placesContainer()[i]
 			place.marker.setVisible(true)
@@ -254,6 +362,9 @@ function initMap(){
 		place.marker.setVisible(boolean)
 	};
 
+	setMapZoom = function(){
+		map.setZoom(12)
+	}
 
 
 
@@ -286,6 +397,7 @@ function myViewModel(){
 
 
 	var self = this
+	//self.userLocation = ko.observable("")
 	self.addressVisible = ko.observable(false)
 	self.infoVisible = ko.observable(false);
 	self.inputValue = ko.observable("");
@@ -293,6 +405,7 @@ function myViewModel(){
 	self.wikiInfo = ko.observable("yay")
 	self.url = ko.observable("")
 	self.goToWikiPage = ko.observable("More Info")
+	self.showMyLocation = ko.observable(true)
 	//self.addressVisible = ko.observable(false)
 	//console.log(markerSelected)
 	self.giveWikiInfo = function(name){
@@ -305,6 +418,7 @@ function myViewModel(){
 		type: "get",
 		url:"https://www.wikipedia.org/w/api.php?action=query&list=search&srsearch="+nameWithUnderscore+"&format=json&callback=wikiCallback",
 		dataType: "jsonp",
+
 	});
 	return wikiPromise
 }
@@ -317,16 +431,12 @@ function myViewModel(){
 
 	self.passClickInformation = function(){
 
-		//console.log("sucess open info")
-		//console.log(this)
-		var name
-		//passTheInfo(this.name())
-		//console.log(markerSelected)
-
 		askInfoToPlacesLibrary(this.marker)
 		self.hideTitles(this.marker)
 		markerSelected(this.marker)
 		self.showWikiInfo(this.marker)
+		self.infoVisible(true)
+		//self.getGeolocation()
 	}
 
 	self.hideTitles = function(marker){
@@ -345,7 +455,7 @@ function myViewModel(){
 	self.showAllTitles = function(){
 		//console.log("sucess on click show All")
 		showAllMarkers()
-
+		self.infoVisible(false)
 
 		for (var i = 0; i < placesContainer().length; i++) {
 
@@ -355,13 +465,24 @@ function myViewModel(){
 
 
 	markerSelected.subscribe(function(marker){
+		//console.log(event)
 		self.hideTitles(marker)
+		console.log(event)
+		self.showWikiInfo(marker)
 	})
 	self.inputValue.subscribe(function(newValue) {
+
+		self.infoVisible(false)
+		setMapZoom()
+
 
 		for(var i = 0; i < placesContainer().length; i++){
 			var place = placesContainer()[i]
 			var regex = /[a-z]/
+
+			console.log(event)
+
+
 			//hideOrShowMarker(true, true)
 			//console.log(self.inputValue().length)
 			if(self.inputValue().length > 0 && self.inputValue().search(regex)>=0){
@@ -402,6 +523,7 @@ function myViewModel(){
 	koAddress.subscribe(function(address){
 		//console.log(address)
 		self.infoVisible(true)
+		console.log(self.infoVisible())
 		self.placeAddress(address);
 
 
@@ -411,16 +533,53 @@ function myViewModel(){
 	self.showWikiInfo = function(marker){
 		wikiPromise = self.giveWikiInfo(marker.title)
 	    wikiPromise.then(function(data){
+	    	console.log(data)
+	    	if(data.query.search.length===0){
+
+	    		alert("Sorry we couldn't find any Wikipedia Information for the place you are looking for")
+	    	}
+	    	else{
 	    		var wikiPlaceInfo = data.query.search[0].snippet
-	    		//console.log(marker.title)
+
 	    		//openInfoWindow(marker, address, wikiPlaceInfo)
 	    		self.wikiInfo(wikiPlaceInfo)
 	    		var wikiUrl = 'https://en.wikipedia.org/wiki/'+marker.title;
 	    		console.log(wikiUrl)
 	    		self.url(wikiUrl)
+	    	}
 
-	})
+		})
 
+	}
+
+	self.getGeolocation = function(){
+		console.log("sucess call to geolocation function")
+		geo = navigator.geolocation
+			var position = geo.getCurrentPosition(positionCall)
+			function positionCall(data){
+			//showDirections(data)
+
+			self.userLocation(data)
+			console.log(data)
+		}
+		 //console.log(position)
+	}
+
+	self.callGoogleDirections = function(){
+		console.log("sucess click take me there")
+		//console.log(markerSelected().position)
+
+		//showDirections(markerSelected)
+
+		geo = navigator.geolocation
+		var position = geo.getCurrentPosition(positionCall)
+		//console.log(position)
+
+		function positionCall(data){
+			console.log(data)
+			showDirections(data)
+
+		}
 
 	}
 
@@ -432,9 +591,3 @@ function myViewModel(){
 
 ko.applyBindings(new myViewModel());
 
-//when clicking a name or marker zoom on this marker and hide other ones, show title hide other ones, show dom with placeinfo
-
- //var name = "Balboa Park"
-//askInfoToPlacesLibrary(name)
-
-//google.maps.event.addDomListener(window, 'load', askInfoToPlacesLibrary)
