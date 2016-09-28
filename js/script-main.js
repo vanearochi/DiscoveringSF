9c5d8a0fc091999787148a9f5a23be7acd1ed08b
Vanessa Arochi <vanearochi@gmail.com>
2016-09-27 18:01:59 -0700

    change loadingdirection position on show directions before alert

--- js/script-main.js
+++ js/script-main.js
@@ -164,12 +164,12 @@ function initMap(){
 	      		loadingDirections(false);
     		}
     		else if(status == google.maps.DirectionsStatus.ZERO_RESULTS){
-    			alert("No route could be found between the origin and destination. Please try another travel mode");
     			loadingDirections(false);
+    			alert("No route could be found between the origin and destination. Please try another travel mode");
     		}
     		else{
-    			alert("Sorry there was an error:" + status);
     			loadingDirections(false);
+    			alert("Sorry there was an error:" + status);
     		}
 		});
     };
